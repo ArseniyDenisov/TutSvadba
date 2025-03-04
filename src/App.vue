f18a7ca9eac30974301e034161072b6b80a55aa7
Ars20000 <nimbanan2014@mail.ru>
2025-03-04 12:12:07 +0300

    Zalupa

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/aa.png"  width="600" height="600"/>
+    
+  
+    
+  </header>
+  <body>
+    <div>
+      
+      <img alt="Vue logo" class="sasha" src="./assets/sasha.png"  width="200" height="220"/>
+     
+      <h1 class="SlovoSPB">
+        Я + саша = секс
+      </h1> 
+      <img alt="Vue logo" class="dima" src="./assets/dima.jpg"  width="200" height="220"/>
+      <div class="PrivetZ">
+      <h1 class="Privet">
+        Узеали этих ребятишек? 
+        Да-да, это мы! Время пролетело так быстро, представляете?
+        И вот мы повзрослели и приняли решение, что пора женится!
+        Приглашаем вас присоединится к нашему первому празднику - 
+        свадбному торжеству!
+        Будем рады, если это событие вы разделите вместе с нами.
+
+      </h1>
+    </div>
+    </div>
+  </body>
+
+  
+</template>
+
