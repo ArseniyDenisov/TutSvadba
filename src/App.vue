875edb75062d2fe2178b81dbef542200d93f698d
Ars20000 <nimbanan2014@mail.ru>
2025-03-13 13:54:32 +0300

    Первый коммит

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,71 @@
 
 </script>
 
+<style>
+ @font-face {
+    font-family: 'Papyrus';
+    src: url('fonts/Papyrus.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+
+</style>
+
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/aa.png"  width="600" height="600"/>
-    
+    <img alt="Vue logo" class="logo" src="./assets/aa.png"  width="530" height="560"/>
+
+    <title>Sasha + Nasta</title>
   
     
   </header>
   <body>
     <div>
       
-      <img alt="Vue logo" class="sasha" src="./assets/sasha.png"  width="200" height="220"/>
+      <img alt="Vue logo" class="sasha" src="./assets/sasha.jpg"  width="220" height="225"/>
      
       <h1 class="SlovoSPB">
-        Я + саша = секс
+        Саша + Настя = <img alt="Vue logo" class="Serdce" src="./assets/Serdce.svg"  width="60" height="66"/>
       </h1> 
-      <img alt="Vue logo" class="dima" src="./assets/dima.jpg"  width="200" height="220"/>
+      
+      <img alt="Vue logo" class="dima" src="./assets/dima.jpg"  width="270" height="220"/>
       <div class="PrivetZ">
-      <h1 class="Privet">
-        Узеали этих ребятишек? 
+        <h1 class="Privet">
+          Узеали этих ребятишек? 
+        </h1>
+        
+      <div class="text-container">
         Да-да, это мы! Время пролетело так быстро, представляете?
         И вот мы повзрослели и приняли решение, что пора женится!
         Приглашаем вас присоединится к нашему первому празднику - 
         свадбному торжеству!
         Будем рады, если это событие вы разделите вместе с нами.
 
+      </div>
+      <h1 class="Privet">
+        СВАДЬБА СОСТОИТСЯ:
       </h1>
+      <h1 class="SpecialPrivet">
+        8 сентября
+      </h1>
+      <h1 class="Privet">
+        C любовь, Александр и Анастасия!
+      </h1>
+      <img alt="Vue logo" class="SiN" src="./assets/s+n.svg"  width="115" height="115"/>
+      <img alt="Vue logo" class="obshiy" src="./assets/sex.png"  width="200" height="220"/>
+      <img alt="Vue logo" class="Timing" src="./assets/Time.svg"  width="500" height="550"/>
+      <h1 class="Privet">Место проведения</h1>
+      <h1 class="text-container">Наша садьба пройдет в Банкетном комплексе "Пять звёзд", шатёр "Зефир".
+        Он находится по адрессу: проспект Максима Горького, 2м, Чебоксары</h1>
+        <img alt="Vue logo" class="Home" src="./assets/Home.svg"  width="100" height="110"/>
+        <h1 class="Privet">Дресс-код</h1>
+        <h1 class="text-container">Для нас главное - вашу присутствие!
+          Но мы будем рады, если вы поддержите стиль нашей свальбы и нашей цветовой палитры:</h1>
+        <h1 class="text-container">Девушки: вечерние платья, костюмы, юбки блузки.</h1>
+        <img alt="Vue logo" class="Man" src="./assets/Woman.jpg"  width="160" height="220"/>
+        <h1 class="Privet">Мужчины: рубашка, брюки, пиджак.</h1>
+        <img alt="Vue logo" class="Man" src="./assets/Man.jpg"  width="160" height="240"/>
     </div>
     </div>
   </body>
